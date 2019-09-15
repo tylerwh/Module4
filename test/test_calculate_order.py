dd91d6314bcc46b2d4dc0427650cb7404a642ee4
Tyler Hochstetler <tylerw.hochstetler@gmail.com>
2019-09-14 23:00:16 -0500

    Added TestCaseTenToLessThanThirty class
    Second set of tests failing currently

--- test/test_calculate_order.py
+++ test/test_calculate_order.py
@@ -30,7 +30,31 @@ class TestCaseUnderTen(unittest.TestCase):
     self.assertEqual(checkout.calculate_order(10, 10, .10), 5.95)
 
 
+class TestCaseTenToLessThanThirty(unittest.TestCase):
 
+
+  def test_sixteen_with_five_dollars_off(self):
+    self.assertEqual(checkout.calculate_order(16, 5, .10), 18.44)
+  
+
+  def test_twentytwo_twentytwo_with_ten_dollars_off(self):
+    self.assertEqual(checkout.calculate_order(22.22, 10, .10), 11.00)
+  
+
+  def test_twenty_with_five_dollars_off(self):
+    self.assertEqual(checkout.calculate_order(20, 5, .15), 21.02)
+  
+
+  def test_twentynine_with_ten_dollars_off(self):
+    self.assertEqual(checkout.calculate_order(29, 10, .20), 24.06)
+
+  
+  def test_twentynine_ninetynine_with_five_dollars_off(self):
+    self.assertEqual(checkout.calculate_order(29.99, 5, .10), 31.79)
+
+  
+  def test_twenty_and_one_cent_with_five_dollars_off(self):
+    self.assertEqual(checkout.calculate_order(20.01, 5, .20), 20.68) 
 
 
 
